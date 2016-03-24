adcd8d7a65d89127b8ef610cbc323867860e00b7
Elliot Smith <elliot.smith91@gmail.com>
2016-03-24 14:23:34 +1000

    Heroku needs

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath homes homes.wsgi --log-file -
---
+++ homes/homes/settings_local.py
@@ -0,0 +1,34 @@
+from tweetvote.settings import *
+
+
+os.environ['HTTPS'] = ""
+
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_HTTPONLY = False
+SECURE_PROXY_SSL_HEADER = () #('HTTP_X_FORWARDED_PROTOCOL', 'https')
+
+DATABASES = {
+        'default': {
+            'NAME': 'tests.db',
+            'ENGINE': 'django.db.backends.sqlite3'
+        }
+}
+
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                },
+            },
+        'loggers': {
+            'api.models': {
+                'handlers': ['console'],
+                'level': 'INFO',
+                },
+            },
+        }
+
+
