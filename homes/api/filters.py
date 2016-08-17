1835f80a3e7274b821c8239bbd537aa3823a0a2e
Elliot Smith <elliot.smith91@gmail.com>
2016-08-17 11:11:48 +1000

    Nearby filter

--- homes/api/filters.py
+++ homes/api/filters.py
@@ -1,6 +1,9 @@
 import django_filters
+import math
 from api.models import Property, Feature
 from rest_framework import filters
+from ast import literal_eval as make_tuple
+from django.db.models import F
 
 class PropertyFilter(filters.FilterSet):
     """Filters used for the Property model.
@@ -58,11 +61,37 @@ class PropertyFilter(filters.FilterSet):
     min_car_spaces = django_filters.NumberFilter(name="car_spaces", lookup_type="gte")
     max_car_spaces = django_filters.NumberFilter(name="car_spaces", lookup_type="lte")
 
+    close_to = django_filters.MethodFilter(action='filter_approx_distance')
+
     class Meta:
         model = Property
         fields = ["min_price", "max_price", "min_bedrooms", "max_bedrooms", 
                 "min_bathrooms", "max_bathrooms", "min_car_spaces", "max_car_spaces",
-                "features"]
+                "features", "close_to"]
 
 
+    def filter_approx_distance(self, queryset, value):
+        """ Filters all results who's address object has a lat long approximatly value[0] from value[1]
+        """
+        # Assume value is in the form (distance, lat, long)
+        try:
+            vals = make_tuple(value)
+        except:
+            # if something bad happened just fallabck to not working for now
+            return queryset
 
+        # remove queryset objects tha have no address
+        queryset = queryset.filter(address_object__isnull=False)
+
+        pi = 3.1415
+        f_lat = pi*(vals[1] - F('address_object__latitude'))/180.0
+        f_long = pi*(vals[2] - F('address_object__longitude'))/180.0
+        m_lat = 0.5*pi*(vals[1] + F('address_object__latitude'))/180.0
+        cosprox = 1 - (m_lat**2)/2.0 # approximate cosine
+        
+        approx_dist = (6371**2)*(f_lat**2 + (cosprox*f_long)**2)
+
+        queryset = queryset.annotate(dist=(approx_dist - vals[0]**2)).annotate(flat=f_lat)
+        queryset = queryset.filter(dist__lte=0)
+
+        return queryset
