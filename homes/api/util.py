f50bc4798bdab6592e35a5c28b4348571e6aea84
Kyle Polich <kyle@datascience.com>
2016-08-23 17:36:34 -0700

    change branding

--- homes/api/util.py
+++ homes/api/util.py
@@ -7,9 +7,9 @@ def send_signup_email(email, link_text):
     result = requests.post(
         "https://api.mailgun.net/v3/" + domain + "/messages",
         auth=("api", settings.MAILGUN_API_KEY),
-        data={"from": "Data Skeptic API <" + settings.MAILGUN_API_FROM + ">",
+        data={"from": "OpenHouse API <" + settings.MAILGUN_API_FROM + ">",
               "to": [email],
-              "subject": "Data Skeptic API Account Confirmation",
+              "subject": "OpenHouse API Account Confirmation",
               "text": "Click to Verify your account: " + link_text})
 
     print(result.text)
